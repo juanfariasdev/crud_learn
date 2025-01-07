772004127a43cc39a94bf5cf8490c186b6c3aed8
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2025-01-06 23:39:18 -0300

    create fake api

---
+++ src/app/api/user/me/route.ts
@@ -0,0 +1,9 @@
+export function GET() {
+
+    return Response.json({
+        "id": "11111",
+        "name": "Juanfarias.dev",
+        "photoUrl": "https://avatars.githubusercontent.com/u/77401614?v=4",
+        "score": 70
+    })
+}
